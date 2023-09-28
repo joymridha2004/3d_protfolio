346378edaabbd7f051876f0644c4aca5e726c82c
JoY <joymridha2004@gmail.com>
2023-09-29 03:20:37 +0530

    complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_s2ad65n',
+        'template_11e9xxh',
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Joy Mridha",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "joymridha2004@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        '_B6O2vEghvCr-WGkh'
       )
       .then(
         () => {
